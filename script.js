c59f6ca45cf12f76a62294768b2141d98a567103
kinoshita <kinoshita062323@gmail.com>
2020-09-23 23:55:36 +0900

    入力処理と入力されたデータの管理方法を変更した

--- script.js
+++ script.js
@@ -29,262 +29,77 @@ const keyMultiply = document.querySelector('.keyMultiply');
 const keyDivide = document.querySelector('.keyDivide');
 const keyEqual = document.querySelector('.keyEqual');
 
-//数字キーの関数を実装
-function one() {            //1番のキーが押されたときに実行する関数
-  display.textContent += 1; //ディスプレイに「1」とテキストを追加
-  switch ( numNumber ) {　　//numNumberで現在何番目のnumを編集中なのか読み取り、
-    case 1:
-      num1 += '1';    //現在編集中のnumに１という文字列を連結する。　　　
-      break;
-    case 2:
-      num2 += '1';
-      break;
-    case 3:
-      num3 += '1';
-      break;
-    case 4:
-      num4 += '1';
+const numberKeys = [key0, key1, key2, key3, key4, key5, key6, key7, key8, key9];
+
+const operatorKeys = [keyPlus, keyMinus, keyMultiply, keyDivide];
+
+// 演算子
+const operators = ['+', '-', '*', '/'];
+
+// 演算子の優先順位を表す
+const defaultOperatorLevel = [1, 1, 2, 2];
+
+let numbers = [];
+let numberIndex = 0;
+
+// 入力された演算子が文字列でここに入る
+let inputOperators = [];
+
+// カッコを使用するときにカッコ一つごとの演算子に対しての重みづけとして使用する
+let operatorWeight = 1;
+
+// inputOperatorsの優先度をここに格納する
+let operatorLevel = [];
+
+
+
+// 数値を入力されたときのイベントリスナー関数を生成する関数
+// 要するにfunction one()といった関数を自動的に作成している
+function setupNumber(n) {
+
+  // クロージャという仕組みを活用している
+  return function(){
+    display.textContent += n;
+    let num = numbers[numberIndex];
+    if(!num){
+      num = n;
+    }else{
+      // 数値を文字列型に変換する
+      num += String(n)
+    }
+    numbers[numberIndex] = num;
+  }
+  
+}
+
+function setupOperator(opIndex){
+
+  return function(){
+    display.textContent += operators[opIndex];
+    inputOperators.push(operators[opIndex]);
+    operatorLevel.push(defaultOperatorLevel[opIndex] * operatorWeight);
+    numberIndex ++;
   }
 }
 
-function two() {
-  display.textContent += 2;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '2';
-      break;
-    case 2:
-      num2 += '2';
-      break;
-    case 3:
-      num3 += '2';
-      break;
-    case 4:
-      num4 += '2';
-  }
-}
+function executeCalc(){
+  console.log(numbers);
+  console.log(inputOperators);
+  console.log(operatorLevel);
 
-function three() {
-  display.textContent += 3;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '3';
-      break;
-    case 2:
-      num2 += '3';
-      break;
-    case 3:
-      num3 += '3';
-      break;
-    case 4:
-      num4 += '3';
-  }
-}
-
-function four() {
-  display.textContent += 4;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '4';
-      break;
-    case 2:
-      num2 += '4';
-      break;
-    case 3:
-      num3 += '4';
-      break;
-    case 4:
-      num4 += '4';
-  }
-}
-
-function five() {
-  display.textContent += 5;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '5';
-      break;
-    case 2:
-      num2 += '5';
-      break;
-    case 3:
-      num3 += '5';
-      break;
-    case 4:
-      num4 += '5';
-  }
-}
-
-function six() {
-  display.textContent += 6;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '6';
-      break;
-    case 2:
-      num2 += '6';
-      break;
-    case 3:
-      num3 += '6';
-      break;
-    case 4:
-      num4 += '6';
-  }
-}
-
-function seven() {
-  display.textContent += 7;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '7';
-      break;
-    case 2:
-      num2 += '7';
-      break;
-    case 3:
-      num3 += '7';
-      break;
-    case 4:
-      num4 += '7';
-  }
-}
-
-function eight() {
-  display.textContent += 8;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '8';
-      break;
-    case 2:
-      num2 += '8';
-      break;
-    case 3:
-      num3 += '8';
-      break;
-    case 4:
-      num4 += '8';
-  }
-}
-
-function nine() {
-  display.textContent += 9;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '9';
-      break;
-    case 2:
-      num2 += '9';
-      break;
-    case 3:
-      num3 += '9';
-      break;
-    case 4:
-      num4 += '9';
-  }
-} 
-
-function zero() {
-  display.textContent += 0;
-  switch ( numNumber ) {
-    case 1:
-      num1 += '0';
-      break;
-    case 2:
-      num2 += '0';
-      break;
-    case 3:
-      num3 += '0';
-      break;
-    case 4:
-      num4 += '0';
-  }  
-}
-
-function zerozero() {
-  display.textContent += '00';
-  switch ( numNumber ) {
-    case 1:
-      num1 += '00';
-      break;
-    case 2:
-      num2 += '00';
-      break;
-    case 3:
-      num3 += '00';
-      break;
-    case 4:
-      num4 += '00';
-  } 
+  
 }
 
 
-//演算子・等号キーの関数を実装
-function plus() {
-  display.textContent += '+';
-  numNumber++;
-  sign = 1;
+for(let i = 0; i < numberKeys.length; i ++){
+  numberKeys[i].addEventListener('click', setupNumber(i));
 }
 
-function minus() {
-  display.textContent += '-';
-  numNumber++;
-  sign = 2;
+for(let i = 0; i < operatorKeys.length; i ++){
+  operatorKeys[i].addEventListener('click', setupOperator(i));
 }
-
-function multiply() {
-  display.textContent += '×';
-  numNumber++;
-  sign = 3;
-}
-
-function divide() {
-  display.textContent += '÷';
-  numNumber++;
-  sign = 4;
-}
-
-function equal() {
-  num1 = Number(num1);  //文字列から整数へ型変換！
-  num2 = Number(num2);
-  num3 = Number(num3);
-  num4 = Number(num4);
-
-  switch( sign ) {   //sign = 1なら加算、sign = 2なら減算を行う
-
-  case 1:
-    sum = num1 + num2 + num3 + num4;
-    break;
-
-  case 2:
-    sum = num1 - num2 - num3 - num4;
-    break;
-
-  case 3:
-    sum = num1 * num2;
-    break;
-
-  case 4:
-    sum = num1 / num2 
-
-  }
-  display.textContent += '=' + sum;
-}
-
-//各キーにそれぞれのEventListenerを追加
-key1.addEventListener('click', one);
-key2.addEventListener('click', two);
-key3.addEventListener('click', three);
-key4.addEventListener('click', four);
-key5.addEventListener('click', five);
-key6.addEventListener('click', six);
-key7.addEventListener('click', seven);
-key8.addEventListener('click', eight);
-key9.addEventListener('click', nine);
-key0.addEventListener('click', zero);
-key00.addEventListener('click', zerozero);
-
-keyPlus.addEventListener('click', plus);
+/*keyPlus.addEventListener('click', plus);
 keyMinus.addEventListener('click', minus);
 keyMultiply.addEventListener('click', multiply);
-keyDivide.addEventListener('click', divide);
-keyEqual.addEventListener('click', equal);
+keyDivide.addEventListener('click', divide);*/
+keyEqual.addEventListener('click', executeCalc);
